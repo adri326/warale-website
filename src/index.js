f282d423fcab478144f0ea3adbf26a59184b0487
Adrien Burgun <adrien.burgun@orange.fr>
2021-07-24 18:48:29 +0200

    :sparkles: Journals support [WIP]

--- src/index.js
+++ src/index.js
@@ -6,25 +6,55 @@ const path = require("path");
 const express = require("express");
 const compression = require("compression");
 const hogan = require("hogan.js"); // A mustache implementation that's fast
+const markdown = require("markdown-it")();
 
 const render = require("./render");
 const config = require("../config.json");
 
 const app = express();
 
-if (!fs.existsSync("generated")){
+if (!fs.existsSync("generated")) {
     fs.mkdirSync("generated");
+}
+
+if (!fs.existsSync("journals")) {
+    fs.mkdirSync("journals");
 }
 
 let header = fs.readFileSync("assets/header.mustache", "utf8");
 let footer = fs.readFileSync("assets/footer.mustache", "utf8");
 let description = fs.readFileSync("assets/description.mustache", "utf8");
+let journals_template = fs.readFileSync("assets/journals.mustache", "utf8");
 
-let template = hogan.compile(header + description + footer);
+let template = hogan.compile(header + description + journals_template + footer);
 
 let partials = {};
 for (let file of fs.readdirSync("assets/partials")) {
     partials[file.replace(/\..*$/g, "")] = hogan.compile(fs.readFileSync(path.join("assets/partials", file), "utf8"));
+}
+
+let journals = [];
+{
+    for (let file of fs.readdirSync("journals/")) {
+        let name = /^(\d+)(?:\s?-)?\s+([a-zA-Z0-9][a-zA-Z0-9\s\-_]*)\.(?:md|txt)$/i.exec(file);
+        if (!name) {
+            console.error("Invalid journal name: " + file);
+            continue;
+        }
+        let year = +name[1];
+        let player = name[2];
+        let raw = fs.readFileSync(path.join("journals", file), "utf8");
+
+        journals.push({
+            year,
+            player,
+            player_sanitized: player.replace(/[^a-zA-Z0-9]/g, "-"),
+            raw,
+            rendered: markdown.render(raw.replace(/^#+/gm, (n) => "#".repeat(Math.min(n.length + 1, 6)))),
+        });
+    }
+
+    journals.sort((a, b) => a.year - b.year);
 }
 
 app.use(compression());
@@ -45,8 +75,11 @@ app.get("/", async (req, res) => {
         title: config.title,
         body_style: {
             main_background: config.style?.main_background ?? "black",
+            secondary_background: config.style?.secondary_background ?? "darkgrey",
             link_color: config.style?.link_color ?? "blue",
-        }
+            summary_color: config.style?.summary_color ?? "blue",
+        },
+        journals
     };
 
     res.write(template.render(context, partials));
